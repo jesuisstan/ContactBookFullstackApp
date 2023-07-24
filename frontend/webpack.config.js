0281e817f287f3dc444da850775abe22a127bf5c
jesuisstan <stan.krivtsov@gmail.com>
2023-07-24 19:57:21 +0200

    webpack.config

---
+++ frontend/webpack.config.js
@@ -0,0 +1,29 @@
+//module.exports = {
+//  devServer: {
+//    allowedHosts: 'all',
+//  },
+//};
+
+import { resolve as _resolve } from 'path';
+
+export const entry = './src/index.tsx';
+export const output = {
+	path: _resolve(__dirname, 'dist'),
+	filename: 'bundle.js', // Replace with your desired output bundle filename
+};
+export const resolve = {
+	extensions: ['.tsx', '.ts', '.js'],
+};
+export const module = {
+	rules: [
+		{
+			test: /\.tsx?$/,
+			use: 'ts-loader',
+			exclude: /node_modules/,
+		},
+	],
+};
+export const devServer = {
+	allowedHosts: 'all',
+};
+
